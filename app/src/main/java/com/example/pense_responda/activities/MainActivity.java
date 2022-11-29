ed45cfc5cdadd212faa9aab043ce0746a5877e03
Kauana Henriques <karoltvbl@gmail.com>
2022-11-29 01:45:24 -0300

    refac local do main

--- app/src/main/java/com/example/pense_responda/activities/MainActivity.java
+++ app/src/main/java/com/example/pense_responda/activities/MainActivity.java
@@ -1,8 +1,10 @@
-package com.example.pense_responda;
+package com.example.pense_responda.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.example.pense_responda.R;
 
 public class MainActivity extends AppCompatActivity {
 
