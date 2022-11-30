58dd77deac37287d02cdce34528d67d3bc89cd91
Kauana Henriques <karoltvbl@gmail.com>
2022-11-30 06:09:36 -0300

    ajusta adapter

--- app/src/main/java/com/example/pense_responda/adapter/Adapter.java
+++ app/src/main/java/com/example/pense_responda/adapter/Adapter.java
@@ -3,7 +3,6 @@ package com.example.pense_responda.adapter;
 
 import android.annotation.SuppressLint;
 import android.content.Context;
-import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -17,91 +16,67 @@ import androidx.navigation.Navigation;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.pense_responda.R;
-import com.example.pense_responda.model.User;
+import com.example.pense_responda.model.Resposta;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
-import java.util.ArrayList;
 import java.util.List;
 
 
 public class Adapter extends RecyclerView.Adapter<Adapter.MyViewHolder> {
-    List<User> listaUsers = new ArrayList<>();
+    List<Resposta> listaRespostas;
     Context context;
-    public Adapter(Context context, List<User> users) {
-        this.context = this.context;
-        this.listaUsers = users;
+    public Adapter(Context context, List<Resposta> respostas) {
+        this.context = context;
+        this.listaRespostas = respostas;
     }
 
     @NonNull
     @Override
     public MyViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
-        //chamado para criar as visualizações - somente as primeiras que aparecem para o usuário
-        //convertendo o XML em uma visualização
-        //cria um objeto do tipo view
-        View itemList = LayoutInflater.from(viewGroup.getContext()).
-                inflate(R.layout.adapter_card_resposta, viewGroup, false);
-        //se usar adapter_card -> ajustar o ViewHolder para usar Button
-        //retorna o itemList que é passado para o construtor da MyViewHolder
+        View itemList = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.adapter_card_resposta, viewGroup, false);
         return new MyViewHolder(itemList);
     }
 
     @Override
     public void onBindViewHolder(@NonNull MyViewHolder myViewHolder, @SuppressLint("RecyclerView") int position) {
-        //exibe os itens no Recycler
-        User u = listaUsers.get(position);
-        myViewHolder.nome.setText(u.getNome());
-        myViewHolder.endereco.setText(u.getEmail());
-        myViewHolder.btnDelete.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                removerItem(position);
-            }
-        });
+        Resposta r = listaRespostas.get(position);
+        myViewHolder.resposta.setText(r.getResposta());
+        myViewHolder.btnDelete.setOnClickListener(v -> removerItem(position));
         Bundle bundle = new Bundle();
-        bundle.putString("NOME", listaUsers.get(position).getNome());
-        bundle.putString("EMAIL", listaUsers.get(position).getEmail());
-        bundle.putString("KEY", listaUsers.get(position).getId());
-       // myViewHolder.btnEdit.setOnClickListener(Navigation.createNavigateOnClickListener(R.id.nav_editarFragment, bundle));
+        bundle.putString("RESPOSTA", listaRespostas.get(position).getResposta());
+        bundle.putString("KEY", listaRespostas.get(position).getId());
+        myViewHolder.btnEdit.setOnClickListener(Navigation.createNavigateOnClickListener(R.id.nav_editarFragment, bundle));
     }
 
     @Override
     public int getItemCount() {
-        //retorna a quantidade de itens que será exibida
-        return listaUsers.size();
+        return listaRespostas.size();
     }
 
     public void removerItem(final int position) {
         new AlertDialog.Builder(context)
-                .setTitle("Deletando pessoa")
+                .setTitle("Deletando resposta")
                 .setMessage("Tem certeza que deseja deletar essa pessoa?")
-                .setPositiveButton("Sim", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialogInterface, int i) {
-                        DatabaseReference reference = FirebaseDatabase.getInstance().getReference("users");
-                        reference.child(listaUsers.get(position).getId()).removeValue();
-                        listaUsers.remove(position);
-                        notifyItemRemoved(position);
+                .setPositiveButton("Sim", (dialogInterface, i) -> {
+                    DatabaseReference reference = FirebaseDatabase.getInstance().getReference("respostas");
+                    reference.child(listaRespostas.get(position).getId()).removeValue();
+                    listaRespostas.remove(position);
+                    notifyItemRemoved(position);
 
-                    }}).setNegativeButton("Não", null).show();
+                }).setNegativeButton("Não", null).show();
     }
 
-    public class MyViewHolder extends RecyclerView.ViewHolder{
-        //dados da pessoa que serão exibidos no recycler
-        TextView nome;
-        TextView endereco;
+    public static class MyViewHolder extends RecyclerView.ViewHolder{
+        TextView resposta;
         ImageButton btnDelete;
         ImageButton btnEdit;
-        //se usar adapter_card -> ajustar o ViewHolder para usar Button
-        //Button btnDelete;
-        //Button btnEdit;
+
         public MyViewHolder(View itemView){
             super(itemView);
-            //passa uma referência para os componentes que estão na interface
-            nome = itemView.findViewById(R.id.textViewPerg);
-            endereco = itemView.findViewById(R.id.textViewResp);
+            resposta = itemView.findViewById(R.id.textViewResp);
             btnDelete = itemView.findViewById(R.id.btnExcluir);
-            btnEdit= itemView.findViewById(R.id.btnEdit);
+            btnEdit= itemView.findViewById(R.id.btnEditar);
         }
     }
 }
