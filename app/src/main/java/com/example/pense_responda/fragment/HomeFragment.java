77f2c7ee9e3e346196026547c1e568792bc2825a
Kauana Henriques <karoltvbl@gmail.com>
2022-11-30 00:34:33 -0300

    add configs

---
+++ app/src/main/java/com/example/pense_responda/fragment/HomeFragment.java
@@ -0,0 +1,23 @@
+package com.example.pense_responda.fragment;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+
+import com.example.pense_responda.R;
+
+
+public class HomeFragment extends Fragment {
+
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+
+        View root = inflater.inflate(R.layout.fragment_home, container, false);
+        return root;
+    }
+}
