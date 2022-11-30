c70dfb448c9f447937e2e4b72ab450955ee80f14
Kauana Henriques <karoltvbl@gmail.com>
2022-11-30 06:13:24 -0300

    adiciona logica de botoes

--- app/src/main/java/com/example/pense_responda/fragment/HomeFragment.java
+++ app/src/main/java/com/example/pense_responda/fragment/HomeFragment.java
@@ -7,6 +7,7 @@ import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
 
 import com.example.pense_responda.R;
 
@@ -14,10 +15,25 @@ import com.example.pense_responda.R;
 public class HomeFragment extends Fragment {
 
 
-    public View onCreateView(@NonNull LayoutInflater inflater,
-                             ViewGroup container, Bundle savedInstanceState) {
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
         View root = inflater.inflate(R.layout.fragment_home, container, false);
+
+        View btnVisualizar = root.findViewById(R.id.buttonVisualizar);
+        btnVisualizar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Navigation.findNavController(getView()).navigate(R.id.action_nav_home_to_nav_responderFragment);
+            }
+        });
+
+        View btnHistorico = root.findViewById(R.id.buttonHistorico);
+        btnHistorico.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Navigation.findNavController(getView()).navigate(R.id.nav_listarFragment);
+            }
+        });
         return root;
     }
 }
